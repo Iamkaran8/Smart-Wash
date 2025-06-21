1f422343a356e43d99011a0bed6e563a1cdc6ff8
karansenthil8 <karanwebdev8@gmail.com>
2025-06-21 10:53:28 +0530

    appRouter is Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
 import './App.css'
+import { Router } from './AppRoute/Router'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Smart wash</h1>
+      <Router />
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return (
+        <>
+            <p className="text-center text-[30px]">Smart Wash</p>
+        </>
+    )
+}
